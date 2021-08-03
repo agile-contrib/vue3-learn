6e1755e736f325f9abb7ee4debf2f74fbbaac905
你好2007 <2501482523@qq.com>
2021-08-03 23:24:58 +0800

    setup基本使用

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  vue3
+  <Index />
 </template>
 
 <script>
-export default {};
-</script>
+/**
+ * 查看的时候，放开需要的部分就可以了
+ */
 
-<style>
-</style>
+import Index from "./pages/setup.vue"; // setup
+
+export default {
+  components: {
+    Index,
+  },
+};
+</script>
---
+++ src/pages/setup.vue
@@ -0,0 +1,29 @@
+<template>
+  {{ msg }}
+  <button @click="doit">点击我</button>
+</template>
+<script>
+// import { h } from "vue";
+
+export default {
+  setup() {
+    let tips = "【温馨提示】";
+
+    // 第一种：返回对象
+    return {
+      // data
+      msg: "这是一段文字",
+
+      //   methods
+      doit() {
+        alert(tips + this.msg);
+      },
+    };
+
+    // 第二种：返回渲染函数
+    // return () => {
+    //   return h("h1", "我是返回的渲染函数哦~");
+    // };
+  },
+};
+</script>
