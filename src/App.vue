3dc501297365bd127f75e4ed69aa90beda82ca85
你好2007 <2501482523@qq.com>
2021-08-04 00:05:06 +0800

    ref定义响应式数据

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
  * 查看的时候，放开需要的部分就可以了
  */
 
-import Index from "./pages/setup.vue"; // setup
+// import Index from "./pages/setup.vue"; // setup
+import Index from "./pages/ref.vue"; // ref定义响应式数据
 
 export default {
   components: {
