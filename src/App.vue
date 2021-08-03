a3228a1e59cd2a40e0ba654225461a3399eabe88
你好2007 <2501482523@qq.com>
2021-08-04 00:20:26 +0800

    reactive定义对象类型数据响应

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,8 @@
  */
 
 // import Index from "./pages/setup.vue"; // setup
-import Index from "./pages/ref.vue"; // ref定义响应式数据
+// import Index from "./pages/ref.vue"; // ref定义响应式数据
+import Index from "./pages/reactive.vue"; // reactive定义对象类型数据响应
 
 export default {
   components: {
